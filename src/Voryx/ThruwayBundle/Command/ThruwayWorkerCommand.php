6b89d28918a71f3d57018345ce83ec8b1565a0e3
GREMILLET Antoine <antoine-externe.gremillet@enedis.fr>
2022-08-24 16:53:06 +0200

    [UPDATE] fix for symfony 5 compatibility

--- src/Voryx/ThruwayBundle/Command/ThruwayWorkerCommand.php
+++ src/Voryx/ThruwayBundle/Command/ThruwayWorkerCommand.php
@@ -2,25 +2,42 @@
 
 namespace Voryx\ThruwayBundle\Command;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Thruway\Peer\Client;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Thruway\Transport\PawlTransportProvider;
 
-class ThruwayWorkerCommand extends ContainerAwareCommand
+class ThruwayWorkerCommand extends Command
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function __construct(
+        ContainerInterface $container
+    ) {
+        $this->container = $container;
+        parent::__construct();
+    }
+
+    private function getContainer() {
+        return $this->container;
+    }
 
     /**
-     * @var \Psr\Log\LoggerInterface $logger
+     * @var LoggerInterface $logger
      */
     private $logger;
 
     /**
      * Called by the Service Container.
      */
-    public function setLogger(\Psr\Log\LoggerInterface $logger)
+    public function setLogger(LoggerInterface $logger)
     {
         $this->logger = $logger;
     }
@@ -84,5 +101,7 @@ class ThruwayWorkerCommand extends ContainerAwareCommand
             $this->logger->critical('EXCEPTION:' . $e->getMessage());
             $output->writeln('EXCEPTION:' . $e->getMessage());
         }
+
+        return \Symfony\Component\Console\Command\Command::SUCCESS;
     }
 }
--- src/Voryx/ThruwayBundle/DependencyInjection/Configuration.php
+++ src/Voryx/ThruwayBundle/DependencyInjection/Configuration.php
@@ -18,8 +18,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode    = $treeBuilder->root('voryx_thruway');
+        $treeBuilder = new TreeBuilder('voryx_thruway');
+        $rootNode    = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
